589760e52e1ea9410b81869254660eef4323fb25
kazi shakerodin <shakerodink@gmail.com>
2022-11-12 14:45:09 +0530

    first commit

---
+++ main5.py
@@ -0,0 +1,14 @@
+# Dictionaries
+
+names = {'Harry': 22,
+         'Shubham': 41,
+         'Jyoti': 19,
+         'Ramdev': 82}
+
+print(names['Ramdev'])
+names['Ramdev'] = 55
+print(names['Ramdev'])
+
+print(names.keys())
+print(names.values())
+
